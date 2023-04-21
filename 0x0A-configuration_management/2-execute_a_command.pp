3113405cfe2683c2bc4b62f9aea29caf37d0d85d
Akanni Emmanuel <akannie33@gmail.com>
2023-04-21 21:00:43 +0100

    Added the path parameter to the exec resource type in the Puppet code to specify the path to the pkill and pgrep commands

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 # kill a process named killmenow
-exec { 'kill-process-killmenow'
+exec { 'kill-process-killmenow':
     command => 'pkill killmenow',
     onlyif  => 'pgrep killmenow',
+    path    => '/usr/bin/',
 }
