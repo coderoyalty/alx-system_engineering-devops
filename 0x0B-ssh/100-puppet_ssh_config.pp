d5ae81f4520744a1cb2aab0898156eb3380e0596
Akanni Emmanuel <akannie33@gmail.com>
2023-04-22 14:10:53 +0100

    fixed

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,5 @@
 # ensure connection to server
-include stdlib
 
-file_line { 'disable password auth':
-    ensure  => present,
-    path    => '/etc/ssh/ssh_config/',
-    line    => '     PasswordAuthentication no',
-    replace => true,
-}
-
-file_line { 'declare identity file':
-  ensure  => present,
-  path    => '/etc/ssh/ssh_config',
-  line    => '     IdentityFile ~/.ssh/school',
-  replace => true,
+exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config' :
+    path => '/bin/'
 }
