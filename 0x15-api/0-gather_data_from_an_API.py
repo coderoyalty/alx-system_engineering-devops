42e207503cb3d3003996cf614f47165c5a7f09ee
Akanni Emmanuel <akannie33@gmail.com>
2023-05-22 20:04:05 +0100

    data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""gather data from an API"""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    if len(argv) > 1:
+        employee = argv[1]
+        url = "https://jsonplaceholder.typicode.com"
+        response = requests.get("{}/users/{}".format(url, employee))
+        data = response.json()
+        employee_name = data.get('name')
+        if employee_name is not None:
+            response = requests.get(
+                 "{}/todos?userId={}".format(url, employee))
+            todos = response.json()
+            completed_todos = []
+            for todo in completed_todos:
+                if todo.get('completed'):
+                    completed_todos.append(todo)
+            completed = len(completed_todos)
+            print("Employee {} is done with tasks({}/{}):".format(
+                employee_name, completed, len(todos)
+            ))
+            for todo in completed_todos:
+                title = todo.get('title')
+                print("\t {}".format(title))
