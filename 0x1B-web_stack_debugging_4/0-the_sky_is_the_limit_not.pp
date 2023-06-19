b3c6de4d2367a7b9d001287de422beca5f5940bc
Akanni Emmanuel <akannie33@gmail.com>
2023-06-19 14:44:19 +0100

    web stack debugging #4: task 0:w

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# increases the no. of traffic an nginx server can handle.
+
+exec { 'fix-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin:/bin/'
+}
+
+-> exec { 'restart':
+  command  => 'nginx restart',
+  path    => '/etc/init.id/'
+}
